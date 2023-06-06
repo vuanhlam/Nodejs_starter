60c3a326f420038f441d5048f62e223b35b60086
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-06 22:57:05 +0700

    Reading and Writing Asynchronously

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
+/**
+ *! Blocking, synchronous way  
+*/
 const textIn =  fs.readFileSync('./txt/input.txt', 'utf-8');
 
 console.log(textIn);
@@ -10,3 +13,24 @@ fs.writeFileSync('./txt/output.txt', textOut);
 
 console.log('File written');
 
+
+
+console.log('-------------------- Non-blocking --------------------------');
+/**
+ *! Non-blocking, asynchronous way  
+*/
+
+// readFile do not have to specify the file encoding 
+// node will start reading this file start.txt in the background as soon as it ready, 
+// it will then start call the callback function 
+fs.readFile('./txt/start.txt', 'utf-8',(error, data1) => {
+    if(error) return console.log('Error');
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+        console.log(data2);
+
+        fs.writeFile('./txt/final.txt', `${data2}\n${data1}`, (error) => {
+            console.log('Your file has been written');
+        })
+    })
+})
+console.log('this line will run before the actual data is ready');
