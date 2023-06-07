fef20ff261efd9319d84de124719a7f28db7ce32
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-07 22:32:37 +0700

    Building a (Very) Simple API ver_2

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const http = require('http');
 const url = require('url')
 
 /**
+ *! we will use the synchronous readFile version 
+ *! synchronous version will block the code execution but in this case it's not a problem
+ *! this code just executed once in the beginning 
+ *! only the callback function of createServer() will executed each time have a new request 
+ *! this block code outside the callback function of createServer() so it will executed once we start the program  
+*/
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+/**
  *! createServer() accept a callback function 
  *! which will be fire each time a new request hit server   
  *! -----------------
@@ -19,12 +28,21 @@ const server = http.createServer((req, res) => {
         res.end('this is PRODUCT page')
     }else if(pathName === '/api') {
 
-        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (error, data) => {
-            res.writeHead(200, {
-                'Content-type': 'application/json',
-            })
-            res.end(data)
-        })
+        /**
+         *! this is not perfect because, each time user hit /api route 
+         *! the file will have to be read and then send back 
+         *! instead we just need read the file once in the beginning 
+         *! and then each time someone hit the route simply send back the data without having to read it each time user request it 
+        */
+        // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (error, data) => {
+        //     res.writeHead(200, {
+        //         'Content-type': 'application/json',
+        //     })
+        //     res.end(data)
+        // })
+
+        // response data of readFileSync
+        res.end(data)
 
     }else {
         res.writeHead(404, {
