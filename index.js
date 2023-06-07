702d5dc86cfd7955890cfe2b9120305636d33a0a
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-07 21:18:25 +0700

    Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
 const fs = require('fs');
+const http = require('http');
 
 /**
- *! Blocking, synchronous way  
+ *! createServer() accept a callback function 
+ *! which will be fire each time a new request hit server   
+ *! -----------------
+ *! callback function access two important variables, it is req and res 
 */
-const textIn =  fs.readFileSync('./txt/input.txt', 'utf-8');
-
-console.log(textIn);
-
-const textOut = `this is what we know about avocado: ${textIn}`
-
-fs.writeFileSync('./txt/output.txt', textOut);
-
-console.log('File written');
-
-
-
-console.log('-------------------- Non-blocking --------------------------');
-/**
- *! Non-blocking, asynchronous way  
-*/
-
-// readFile do not have to specify the file encoding 
-// node will start reading this file start.txt in the background as soon as it ready, 
-// it will then start call the callback function 
-fs.readFile('./txt/start.txt', 'utf-8',(error, data1) => {
-    if(error) return console.log('Error');
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-        console.log(data2);
-
-        fs.writeFile('./txt/final.txt', `${data2}\n${data1}`, (error) => {
-            console.log('Your file has been written');
-        })
-    })
+const server = http.createServer((req, res) => {
+    console.log(res);
+    res.end('hello from the server');
 })
-console.log('this line will run before the actual data is ready');
+
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('the server this listen in port 8000');
+})
+
