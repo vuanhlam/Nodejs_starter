5e2a7535dda64a6aac147836037afc099c647c0a
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-09 23:13:00 +0700

    First make API GET Request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 /**
  *! express() function will add a bunch of function to variable app 
@@ -6,17 +7,23 @@ const express = require('express');
 const app = express();
 
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
 /**
- *! routing determine how the application response to a certain client request or url
- *! not only response to url also with http method which is use for that request.
+ *! ==> /api/v1/tour' : end point
+ *! (req, res) => {} : route handler, this callback function will run inside the event loop 
 */
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'hello from the server!'})
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 })
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint')
-})
 
 const port = 3000;
 app.listen(port, () => {
