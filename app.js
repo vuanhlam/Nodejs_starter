2e2a6d24a8b074363bae3a2175161d1a0be7530f
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-10 21:06:35 +0700

    First make API POST Request

--- app.js
+++ app.js
@@ -2,30 +2,49 @@ const express = require('express');
 const fs = require('fs');
 
 /**
- *! express() function will add a bunch of function to variable app 
-*/
+ *! express() function will add a bunch of function to variable app
+ */
 const app = express();
+app.use(express.json());
 
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 /**
  *! ==> /api/v1/tour' : end point
- *! (req, res) => {} : route handler, this callback function will run inside the event loop 
-*/
+ *! (req, res) => {} : route handler, this callback function will run inside the event loop
+ */
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours
-        }
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
 
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = [...tours, { newID, ...req.body }];
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTour),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+            newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-})
+  console.log(`App running on port ${port}...`);
+});
