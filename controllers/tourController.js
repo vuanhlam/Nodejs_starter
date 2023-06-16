fb58569c15219f0d4b5f857ac9c386a43e30c6fc
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-16 09:59:30 +0700

    Param Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,18 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  } 
+  next();
+}
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -37,13 +49,6 @@ exports.createTour = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
 
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   const tour = tours.find((item) => item.id === id);
 
   res.status(200).json({
@@ -55,13 +60,6 @@ exports.getTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -71,13 +69,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,9 +5,21 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  checkID
 } = require('./../controllers/tourController');
 
 const router = express.Router();
+
+/**
+ *! Params Middleware is Middleware only run for certain parameter,
+ *! basically when we have a certain parameter in the url like: /api/tours/:parameter
+ */
+
+/**
+ *! in the param middleware function we acutally access to the fourth argument
+ *! that one is the value of parameter  
+*/
+router.param('id', checkID);
 
 router.route('/').get(getAllTours).post(createTour);
 
