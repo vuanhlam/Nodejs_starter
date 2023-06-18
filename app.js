115234a73396259abc0e46e60d543f9bbe16e6af
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-18 09:20:27 +0700

    environment variables

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const fs = require('fs'); 
+const fs = require('fs');
 const morgan = require('morgan');
-const userRouter = require('./routes/userRoutes')
-const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARE
 /**
- *! app.js is usually mainly use for Middleware declaration 
- *! we have all the Middleware that we want to apply to all the routes 
-*/
+ *! app.js is usually mainly use for Middleware declaration
+ *! we have all the Middleware that we want to apply to all the routes
+ */
 app.use(express.json());
-app.use(morgan('dev'));
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -23,16 +27,14 @@ app.use((req, res, next) => {
    */
   next();
 });
- 
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-
 // 2) ROUTER
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const dotenv = require('dotenv');
+
+/**
+ *! dotenv variable will read our config.env file
+ *! and save the variables into node.js environment variables
+ */
+dotenv.config({ path: './config.env' }); // define config before app because we need to read the Environment variables before the app starts
 const app = require('./app');
 
-const port = 3000;
+/**
+ *! environment variables are global variables that are used to define the evironment in which is node.js app is running
+ *! but nodejs itself actually set a lot of environment variables
+*/
+// console.log(app.get('env')); // this is set by express
+
+/**
+ *! this is the environment which is set by nodejs 
+*/
+ console.log(process.env); // this is set by nodejs
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
