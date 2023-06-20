6e830777d6e5be7acfadb7c69760dcc6b9b5a620
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-20 22:00:08 +0700

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -20,42 +20,58 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DB connection successful!');
   });
 
-
 /**
- *! - mongoose is all about Modal, a Modal is like a blue print (bản thiết kế) 
- *! - like a classes in Javascript 
- *! - We create a Modal and mongoose in order to create a document using it 
- *! - to query, update and delete document 
- ** to create a Modal we actually need a Schema 
- ** we use this Schema to describle data, to set default value, to validate the data 
-*/
+ *! - mongoose is all about Modal, a Modal is like a blue print (bản thiết kế)
+ *! - like a classes in Javascript
+ *! - We create a Modal and mongoose in order to create a document using it
+ *! - to query, update and delete document
+ ** to create a Modal we actually need a Schema
+ ** we use this Schema to describle data, to set default value, to validate the data
+ */
 
 // create Schema
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     require: [true, 'A tour must have a name'],
-    unique:true
+    unique: true,
   },
   rating: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
   price: {
     type: Number,
-    require: [true, 'A tour must have a price']
-  }
-})
+    require: [true, 'A tour must have a price'],
+  },
+});
 
 //create Modal
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'Son dong case tour VIP1',
+  rating: 5,
+  price: 9000000,
+});
+
+/**
+ *! this will save to the tours Collections in the database
+ */
+testTour
+  .save()
+  .then((document) => {
+    console.log(document);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
