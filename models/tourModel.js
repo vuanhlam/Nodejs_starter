761c031a95afb5239acf9e61e7f2efafbf82fa24
Vu Anh Lam <vuanhlam000@gmail.com>
2023-06-22 21:13:39 +0700

    Modeling The Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,26 @@ const tourSchema = new mongoose.Schema({
     require: [true, 'A tour must have a name'],
     unique: true,
   },
+  duration: {
+    type: Number,
+    require: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    require: [true, 'A tour must have a group size']
+  },
+  diffyculty: {
+    type: String,
+    require: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
   rating: {
     type: Number,
     default: 4.5,
@@ -15,6 +35,30 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     require: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    // trim only work for String value
+    trim: true,
+    require: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    require: [true, 'A tour must have a cover image']
+  },
+  // because we have multiple images and we want to save this images as an Array of String, just define like this [String]
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  // the tour can be start in diffent date 
+  startDates: [Date]
+
 });
 
 //create Modal
