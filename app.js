a4747c2ed5c3ef624e7241a0fd2fb0fe37405e30
Vu Anh Lam <vuanhlam000@gmail.com>
2023-07-02 16:19:59 +0700

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
-const morgan = require('morgan'); 
+const morgan = require('morgan');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the Middleware');
+  req.requestTime = new Date().toISOString();
   /**
    *! if don't call next() the request/response cycle will be stuck in this point
    *! we would not be able to move on and never can send back the response to the client
@@ -28,13 +28,20 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
 // 2) ROUTER
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+/**
+ *! app.all() is a special method that is going to run for all the http methods
+ *! that is get, post, patch, delete, etc
+ ** this should be the last middleware
+ */
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
