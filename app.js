07654ae9daee98519fe8b08e0523ca39e8106702
Vu Anh Lam <vuanhlam000@gmail.com>
2023-07-02 17:30:23 +0700

    Global Error Handling Middlware

--- app.js
+++ app.js
@@ -38,10 +38,41 @@ app.use('/api/v1/users', userRouter);
  ** this should be the last middleware
  */
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail'
+  err.statusCode = 404;
+
+  /**
+   *! this time we use next in a special way
+   ** We need to pass the error in to next()
+   *! so if the next function receipt an argument no matter what it is 
+   *! Express will automaticlly know that there were an error 
+   * TODO - whenever we pass anything into next it will assump that it is an error, 
+   * TODO - it will then skipp all the orther middleware in the Midllware stack and
+   * TODO - send the error that we pass in to global error hanlding Middleware  
+  */
+  next(err)
+});
+
+
+/**
+ *! Error Handling Middleware have four arguments
+ *! By specify four parameters Express automatically recognized as an error handling middleware 
+ *! There for only call it when there is an error 
+*/
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error'
+  
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
 });
 
 module.exports = app;
