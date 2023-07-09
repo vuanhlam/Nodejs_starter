987def6696bbe809ec3006ae76325a5f013de595
Vu Anh Lam <vuanhlam000@gmail.com>
2023-07-09 17:30:07 +0700

    Singing up Users, create token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModal');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  //   const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  /**
+   *!
+   */
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
