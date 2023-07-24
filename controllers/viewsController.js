d3a90a5e24eea2f35bd921ebe5b05ff84412ed83
Vu Anh Lam <vuanhlam000@gmail.com>
2023-07-24 16:37:30 +0700

    Building the Login Screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -18,14 +18,18 @@ exports.getTour = catchAsync(async (req, res) => {
   // 1) get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user'
+    fields: 'review rating user',
   });
 
   // 2) Build template
 
   // 3) Render template using data from step 1
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: tour.name,
     tour,
   });
+});
+
+exports.login = catchAsync(async (req, res) => {
+  res.status(200).render('login', {title: 'Log into your account'});
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getOverview, getTour } = require('../controllers/viewsController');
+const { getOverview, getTour, login } = require('../controllers/viewsController');
 
 const router = express.Router();
 
 router.get('/', getOverview);
 
 router.get('/tour/:slug', getTour);
+
+router.get('/login', login)
 
 module.exports = router;
