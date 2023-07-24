8610e579e2ce73cd1b431336c1a163805fe00da5
Vu Anh Lam <vuanhlam000@gmail.com>
2023-07-24 15:17:07 +0700

    Building the Tour Page - Part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,12 +10,22 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 3) Render template using tour data from step 1
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours
+    tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
+  // 2) Build template
+
+  // 3) Render template using data from step 1
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
